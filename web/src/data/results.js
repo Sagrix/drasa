98079a6c262e20cfba74593d4aed4e9b739b492a
moustafa <spaceflyby@gmail.com>
2018-03-10 18:45:27 -0330

    Added past results

---
+++ web/src/data/results.js
@@ -0,0 +1,22 @@
+const results = [
+    {
+        "id" : 1,
+        "condition" : 'HIV',
+        "date" : "12/01/2017",
+        "result" : 'positive'
+    },
+    {
+        "id" : 2,
+        "condition" : 'Chlamydia',
+        "date" : "21/06/2017",
+        "result" : 'negative'
+    },
+    {
+        "id" : 3,
+        "condition" : 'Syphillis',
+        "date" : "05/12/2017",
+        "result" : 'negative'
+    }
+]
+
+export default results
